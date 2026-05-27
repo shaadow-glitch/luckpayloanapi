23301f7d325e5914f92e753dd47c0facc36df86c
shaadow-glitch <shaktipani3@gmail.com>
2026-05-27 15:12:00 +0530

    Loggers added

--- src/main/java/com/example/luckpayapi/service/LoanServiceImpl.java
+++ src/main/java/com/example/luckpayapi/service/LoanServiceImpl.java
@@ -6,10 +6,12 @@ import com.example.luckpayapi.dto.LoanResponseDto;
 import com.example.luckpayapi.entity.LoanAccount;
 import com.example.luckpayapi.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class LoanServiceImpl implements LoanService{
 
     private final LoanApiClient loanApiClient;
@@ -17,17 +19,21 @@ public class LoanServiceImpl implements LoanService{
 
     @Override
     public LoanResponseDto getLoanDetails(String loanAccountNumber) {
-
+        log.info("Fetching loan details for account number: {}",
+                loanAccountNumber);
         LoanApiResponseDto apiResponse = loanApiClient.fetchLoanDetails(loanAccountNumber);
 
+        log.info("Received response from integration layer");
         LoanAccount loanAccount = new LoanAccount();
 
         loanAccount.setLoanAccountNumber(apiResponse.getLoanAccountNumber());
         loanAccount.setDueDate(apiResponse.getDueDate());
         loanAccount.setEmiAmount(apiResponse.getEmiAmount());
 
+        log.info("Saving loan details into database");
         loanRepository.save(loanAccount);
 
+        log.info("Loan details saved successfully");
         LoanResponseDto responseDto = new LoanResponseDto();
 
         responseDto.setLoanAccountNumber(loanAccount.getLoanAccountNumber());
